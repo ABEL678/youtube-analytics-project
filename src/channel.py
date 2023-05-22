9ccb0600b1d030064d40b2de0da1be8211c6ff41
ABEL <abelovearn@gmail.com>
2023-05-23 00:59:51 +0300

    made homework-2

--- src/channel.py
+++ src/channel.py
@@ -1,7 +1,6 @@
 import json
 import os
 from googleapiclient.discovery import build
-import isodate
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
 api_key: str = os.getenv('YT_API_KEY')
@@ -20,9 +19,35 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self._channel_id = channel_id
+        channel = youtube.channels().list(id=self._channel_id, part='snippet,statistics').execute()
+        self.title = channel['items'][0]['snippet']['title']
+        self.description = channel['items'][0]['snippet']['description']
+        self.url = f'https://www.youtube.com/channel/{channel_id}'
+        self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.view_count = channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = youtube.channels().list(id=self._channel_id, part='snippet,statistics').execute()
         printj(channel)
+
+    @classmethod
+    def get_service(cls):
+        # создать специальный объект для работы с API
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    def to_json(self, file_name: str):
+        channel_attr = {'channel_id': f'{self._channel_id}',
+                        'title': f'{self.title}',
+                        'description': f'{self.description}',
+                        'url': f'{self.url}',
+                        'subscriberCount': f'{self.subscriber_count}',
+                        'videoCount': f'{self.video_count}',
+                        'viewCount': f'{self.view_count}'
+                        }
+
+        with open(f'{file_name}', 'w') as file:
+            json.dump(channel_attr, file, indent=2, ensure_ascii=False)
