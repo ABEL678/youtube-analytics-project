f28730f25a56147bccc6f4d42a2b27f16ecedd43
ABEL <abelovearn@gmail.com>
2023-06-11 16:07:03 +0300

    made homework-6

--- src/video.py
+++ src/video.py
@@ -14,13 +14,20 @@ class Video:
     https://www.youtube.com/watch?v= или https://youtu.be/
     """
     def __init__(self, video_id: str) -> None:
-        self.video_id = video_id
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id).execute()
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.url_video = f'https://youtu.be/{video_id}'
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_id = video_id
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id).execute()
+            self.video_title: str = video_response['items'][0]['snippet']['title']
+            self.url_video = f'https://youtu.be/{video_id}'
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_id = video_id
+            self.video_title = None
+            self.url_video = None
+            self.view_count: None
+            self.like_count: None
 
     def __str__(self):
         return f"{self.video_title}"
